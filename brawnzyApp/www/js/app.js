d1d2761c11295d4bc718b4117ec3c9d229cee809
Andrew Mestas <andrew.mestas.dev@gmail.com>
2015-12-17 09:58:30 -0800

    ionic app

---
+++ brawnzyApp/www/js/app.js
@@ -0,0 +1,166 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic',
+                           'starter.controllers',
+                           'starter.services',
+                           'auth0',
+                           'angular-storage',
+                           'angular-jwt'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider, authProvider, $httpProvider,
+  jwtInterceptorProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+    // This is the state where you'll show the login
+    // .state('login', {
+    //   url: '/login',
+    //   templateUrl: 'templates/login.html',
+    //   controller: 'LoginCtrl',
+    // })
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+      //  data: {
+      //   requiresLogin: true
+      // }
+  })
+
+
+  .state('tab.home', {
+    url: '/home',
+    views: {
+      'tab-home': {
+        templateUrl: 'templates/tab-home.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+
+    .state('tab.nearme', {
+    url: '/nearme',
+    views: {
+      'tab-nearme': {
+        templateUrl: 'templates/tab-nearme.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+
+  .state('tab.newworkout', {
+    url: '/newworkout',
+    views: {
+      'tab-newworkout': {
+        templateUrl: 'templates/tab-newworkout.html',
+        controller: 'workoutFormCtrl'
+
+      }
+    }
+  })
+  // Each tab has its own nav history stack:
+
+  .state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+
+  .state('tab.chats', {
+      url: '/chats',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/tab-chats.html',
+          controller: 'ChatsCtrl'
+        }
+      }
+    })
+    .state('tab.chat-detail', {
+      url: '/chats/:chatId',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/chat-detail.html',
+          controller: 'ChatDetailCtrl'
+        }
+      }
+    })
+
+  .state('tab.account', {
+    url: '/account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/dash');
+  $httpProvider.defaults.headers.common = {};
+  $httpProvider.defaults.headers.post = {};
+  $httpProvider.defaults.headers.put = {};
+  $httpProvider.defaults.headers.patch = {};
+
+   // authProvider.init({
+   //    domain: 'brawnzy.auth0.com',
+   //    clientID: '',
+   //    callbackURL: location.href,
+   //    loginState: 'login'
+   //  }); 
+
+    jwtInterceptorProvider.tokenGetter = function(store, jwtHelper, auth) {
+    var idToken = store.get('token');
+    var refreshToken = store.get('refreshToken');
+    // If no token return null
+    if (!idToken || !refreshToken) {
+      return null;
+    }
+    // If token is expired, get a new one
+    if (jwtHelper.isTokenExpired(idToken)) {
+      return auth.refreshIdToken(refreshToken).then(function(idToken) {
+        store.set('token', idToken);
+        return idToken;
+      });
+    } else {
+      return idToken;
+    }
+  }
+
+  // $httpProvider.interceptors.push('jwtInterceptor');
+
+})
+.run(function(auth) {
+  // Hook auth0-angular to all the events it needs to listen to
+  auth.hookEvents();
+})
---
+++ brawnzyApp/www/js/controllers.js
@@ -0,0 +1,116 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope, WorkoutApi) {
+    $scope.response = null;
+
+    WorkoutApi.get(function success(d) {
+      $scope.response = d;
+    }, function error(d) {
+      $scope.response = d;
+    });
+  
+})
+
+.controller('workoutFormCtrl', function($scope, $state, WorkoutApi, $http, $httpParamSerializerJQLike) {
+
+  $scope.formData = { 'set_amount' : 0,
+                      'day_index' : 0};
+  $scope.message = "";
+  $scope.data;
+  $scope.response = null;
+
+  $scope.range = function(min, max, step) {
+        // parameters validation for method overloading
+        if (max == undefined) {
+            max = min;
+            min = 0;
+        }
+        step = Math.abs(step) || 1;
+        if (min > max) {
+            step = -step;
+        }
+        // building the array
+        var output = [];
+        for (var value=min; value<max; value+=step) {
+            output.push(value);
+        }
+        // returning the generated array
+        return output;
+    };
+
+  $scope.submit = function(){
+     // $state.go('tab.home');
+     if( 
+       $scope.formData['set_amount'] && $scope.formData['day_index'] != undefined 
+       && $scope.formData['weekday'] && $scope.formData['weekly'] != undefined && $scope.formData['weight'] != undefined
+
+      ){
+     $scope.message = "sending..."
+     $scope.data = $scope.formData;
+
+      $http({
+      method: "POST",
+      url: "https://rocky-oasis-8496.herokuapp.com/api/workout",
+      headers: {
+              'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+                },
+      data: $httpParamSerializerJQLike($scope.data)
+      }).success(
+          function(responseData) {
+            $scope.response = responseData;
+            $state.go('tab.dash');
+          }
+      );
+    } else {
+      $scope.message = "Please fill out required forms";
+    }
+  }
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+})
+.controller('LoginCtrl', function($scope, auth, $state, store) {
+  auth.signin({
+    authParams: {
+      // This asks for the refresh token
+      // So that the user never has to log in again
+      scope: 'openid offline_access',
+      // This is the device name
+      device: 'Mobile device'
+    },
+    // Make the widget non closeable
+    standalone: true
+  }, function(profile, token, accessToken, state, refreshToken) {
+          // Login was successful
+    // We need to save the information from the login
+    store.set('profile', profile);
+    store.set('token', token);
+    store.set('refreshToken', refreshToken);
+    $state.go('tab.dash');
+  }, function(error) {
+    // Oops something went wrong during login:
+    console.log("There was an error logging in", error);
+  });
+});
